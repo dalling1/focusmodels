1a6c8ff418ce859d3581cc23f4c5c20d8f560c34
David Allingham <David.Allingham@newcastle.edu.au>
2020-10-08 15:39:06 +1100

    Cleaned up console logging code in automorphism.js.

--- js/automorphism.js
+++ js/automorphism.js
@@ -13,14 +13,9 @@ function automorphism(node1,node2){
  for (var i=0;i<nodeAddress.length;i++){
   oldaddr = nodeAddress[i];
   newaddr[i] = collapseAddress(fromaddr + reverseString(toaddr) + oldaddr);
-//  console.log(oldaddr + " -> " + newaddr);
  }
 
  nodeAddress = newaddr;
-
- // old way:
-// V0[0] = nodeAddress[0];
-// V0[1] = nodeAddress[1];
 
  var params = new FocusModel;
  params.getCurrent();
